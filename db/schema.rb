4357c40a10fce927f2f17d4ecdb3bacd65b12fb5
TairaMasaru <taira@TairaMasaru-no-MacBook-Air.local>
2017-10-19 13:46:16 +0900

    remove user scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -37,11 +37,4 @@ ActiveRecord::Schema.define(version: 20171019041540) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
