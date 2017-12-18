75f5f6f94f4e5071d51948fb17fc6beb4f3773c4
satomicchy <satomi_t_sora@yahoo.co.jp>
2017-12-19 04:28:15 +0900

    set root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :tags, except: :show
   resources :books
   resources :products, only: [:index, :show]
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
