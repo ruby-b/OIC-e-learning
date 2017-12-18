ab0494c28d3e7af392f3080d8a940e455b329df0
satomicchy <satomi_t_sora@yahoo.co.jp>
2017-12-18 16:41:20 +0900

    set design and assets front

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 class ProductsController < ApplicationController
   def index
+    @products = Book.all
+    render layout: 'front'
   end
 
   def show
+    @products = Book.find(params[:id])
   end
 end
