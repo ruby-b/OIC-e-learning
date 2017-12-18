dbbc4bff612c21cca1d5b4d0560931457ac5a3d8
satomicchy <satomi_t_sora@yahoo.co.jp>
2017-12-18 15:16:45 +0900

    generate products controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :tags, except: :show
   resources :books
+  resources :products, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
