5ab37a5a475fa5804332a416185ceb0ba2891823
satomicchy <satomi_t_sora@yahoo.co.jp>
2017-12-20 15:24:11 +0900

    add status to Order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218194551) do
+ActiveRecord::Schema.define(version: 20171220062237) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20171218194551) do
     t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0, null: false
     t.index ["book_id"], name: "index_orders_on_book_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
