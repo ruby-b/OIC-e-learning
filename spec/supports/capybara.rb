b3108481a8e65fe0e3952f96f13966c5401e4958
satomicchy <satomi_t_sora@yahoo.co.jp>
2017-12-24 00:17:25 +0900

    80 characters per line

--- spec/supports/capybara.rb
+++ spec/supports/capybara.rb
@@ -2,13 +2,14 @@ require 'capybara/rspec'
 require 'selenium-webdriver'
 
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app,
-                                 browser: :chrome,
-                                 desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-                                   chrome_options: {
-                                     args: %w(headless disable-gpu window-size=1680,1050),
-                                   },
-                                 )
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+      chrome_options: {
+        args: %w(headless disable-gpu window-size=1680,1050),
+      },
+    )
   )
 end
 
