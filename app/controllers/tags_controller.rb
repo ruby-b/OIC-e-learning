e4e1d80c69d7593d50570213c106a81937f69f73
satomicchy <satomi_t_sora@yahoo.co.jp>
2017-12-23 18:34:04 +0900

    80 characters per line

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -23,7 +23,8 @@ class TagsController < ApplicationController
 
     respond_to do |format|
       if @tag.save
-        format.html { redirect_to tags_path, notice: 'Tag was successfully created.' }
+        format.html { redirect_to tags_path,
+                                 notice: 'Tag was successfully created.' }
         format.json { render :show, status: :created, location: @tag }
       else
         format.html { render :new }
@@ -37,7 +38,8 @@ class TagsController < ApplicationController
   def update
     respond_to do |format|
       if @tag.update(tag_params)
-        format.html { redirect_to tags_path, notice: 'Tag was successfully updated.' }
+        format.html { redirect_to tags_path,
+                                  notice: 'Tag was successfully updated.' }
         format.json { render :show, status: :ok, location: @tag }
       else
         format.html { render :edit }
@@ -51,7 +53,8 @@ class TagsController < ApplicationController
   def destroy
     @tag.destroy
     respond_to do |format|
-      format.html { redirect_to tags_url, notice: 'Tag was successfully destroyed.' }
+      format.html { redirect_to tags_url,
+                                notice: 'Tag was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/views/books/_book.json.jbuilder
+++ app/views/books/_book.json.jbuilder
@@ -1,2 +1,3 @@
-json.extract! book, :id, :title, :author, :published_on, :showing, :price, :created_at, :updated_at
+json.extract! book, :id, :title, :author, :published_on, :showing, :price,
+              :created_at, :updated_at
 json.url book_url(book, format: :json)
